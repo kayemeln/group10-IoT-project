cfd0f3e6999e5d7acf7cfbef496e8924fff5d72c
LiamRyan23 <ryanl23@tcd.ie>
2026-02-23 15:11:01 +0000

    Code to upload to esp32 http server

---
+++ web_2/main/app_main.cpp
@@ -0,0 +1,23 @@
+#include "who_camera.h"
+#include "who_human_face_detection.hpp"
+#include "app_wifi.h"
+#include "app_httpd.hpp"
+#include "app_mdns.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+
+static QueueHandle_t xQueueAIFrame = NULL;
+static QueueHandle_t xQueueHttpFrame = NULL;
+
+extern "C" void app_main()
+{
+    app_wifi_main();
+
+    xQueueAIFrame = xQueueCreate(2, sizeof(camera_fb_t *));
+    xQueueHttpFrame = xQueueCreate(2, sizeof(camera_fb_t *));
+
+    register_camera(PIXFORMAT_RGB565, FRAMESIZE_QVGA, 2, xQueueHttpFrame);
+    app_mdns_main();
+    // register_human_face_detection(xQueueAIFrame, NULL, NULL, xQueueHttpFrame);
+    register_httpd(xQueueHttpFrame, NULL, true);
+}
